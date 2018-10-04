44f1a04a9428918286a0fe4d51ef612e80fe8ca0
dbx92 <davidpiobergamo@comcast.net>
2018-10-04 01:46:54 -0400

    searching_complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,19 +5,19 @@ class AddressBook
   attr_reader :entries
 
   def initialize
-     @entries = []
+    @entries = []
   end
 
   def add_entry(name,phone_number,email)
 
-      index = 0
-      entries.each do |entry|
-        if name < entry.name
-          break
-        end
-        index += 1
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
       end
-      entries.insert(index, Entry.new(name, phone_number, email))
+      index += 1
+    end
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
 
   def remove_entry(name, phone_number, email)
@@ -25,17 +25,44 @@ class AddressBook
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
         entries.delete(entry)
       end
+    end
   end
-end
 
-def import_from_csv(file_name)
-      # Implementation goes here
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-   # #8
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-     end
-   end
- end
+  def import_from_csv(file_name)
+    # Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # #8
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower+upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      else
+        lower = mid + 1
+      end
+    end
+    return nil
+  end
+
+  def iterative_search(name)
+    entries.each do |e|
+      return e if e.name == name
+    end
+    return nil
+  end
+
+end
